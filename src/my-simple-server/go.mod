8b81a44fdb9d813a79826638e8741e50b104eb55
Noobi <xpectuer223@gmail.com>
2020-12-11 00:14:48 +0800

    2020.12.11

--- src/my-simple-server/go.mod
+++
@@ -1,9 +0,0 @@
-module hello
-
-go 1.15
-
-require (
-	github.com/gohouse/converter v0.0.3 // indirect
-	github.com/gohouse/gorose v1.0.5 // indirect
-	github.com/Xpectuer/mircro-service/tree/master/src
-)
--- src/my-simple-server/main.go
+++ src/my-simple-server/main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
-	"handlers"
 	"log"
+
 	"net/http"
 	"os"
+
+	"github.com/Xpectuer/mircro-service/src/my-simple-server/handlers"
 )
 
 func main() {
-	// make things happen
-	// http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-	//
-	// })
-	// log.New( where to out put(file or stdout), prefix string, flags )
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
+	/**
+	Serve Mux  is a map spcifies
+	the routers and handler funcs
+	*/
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
 
 	log.Println("Starting Server")
-	err := http.ListenAndServe(":9091", nil)
+	err := http.ListenAndServe(":9091", sm)
 	log.Fatal(err)
 }
